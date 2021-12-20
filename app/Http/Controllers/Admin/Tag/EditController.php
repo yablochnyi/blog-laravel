67ffddadd665f762b55b6926a8f3070d3aa9272f
Artem <artem.yablochnyi@yandex.ru>
2021-12-20 21:06:48 +0300

    edit categories to category

--- app/Http/Controllers/Admin/Tag/EditController.php
+++ app/Http/Controllers/Admin/Tag/EditController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Admin\Category;
+namespace App\Http\Controllers\Admin\Tag;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
--- app/Http/Controllers/Admin/Tag/ShowController.php
+++ app/Http/Controllers/Admin/Tag/ShowController.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace App\Http\Controllers\Admin\Category;
+namespace App\Http\Controllers\Admin\Tag;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
-class EditController extends Controller
+class ShowController extends Controller
 {
     public function __invoke(Category $category)
     {
-        return view('admin.category.edit', compact('category'));
+        return view('admin.category.show', compact('category'));
     }
 }
