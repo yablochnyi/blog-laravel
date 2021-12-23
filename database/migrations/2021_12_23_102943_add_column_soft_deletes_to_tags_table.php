c638d63abe75c5c44978df402494646c62ad2c51
Artem <artem.yablochnyi@yandex.ru>
2021-12-23 15:05:45 +0300

    add softdelet for tags

--- database/migrations/2021_12_23_102943_add_column_soft_deletes_to_tags_table.php
+++ database/migrations/2021_12_23_102943_add_column_soft_deletes_to_tags_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnSoftDeletesToCategoriesTable extends Migration
+class AddColumnSoftDeletesToTagsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class AddColumnSoftDeletesToCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::table('categories', function (Blueprint $table) {
+        Schema::table('tags', function (Blueprint $table) {
             $table->softDeletes();
         });
     }
@@ -25,7 +25,7 @@ class AddColumnSoftDeletesToCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::table('categories', function (Blueprint $table) {
+        Schema::table('tags', function (Blueprint $table) {
             $table->dropSoftDeletes();
         });
     }
